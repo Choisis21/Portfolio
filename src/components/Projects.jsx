69c54408cd386ccdc4f9aad368a87fabe8b4bfd9
Choisis21 <chosenakinnola@gmail.com>
2026-04-24 06:28:59 +0100

    Project card fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -155,7 +155,7 @@ const Projects = () => {
       ) : (
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {filteredItems.map((item, index) => {
-            const showLightboxIcon = index === 5;
+            const showLightboxIcon = !item.link;
 
             return (
               <motion.div
